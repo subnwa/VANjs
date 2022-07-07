b8111009fd4ae0f2aca83441b542e5a710039492
nwa <nwa.bio.web@gmail.com>
2022-07-07 20:17:19 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    
+    Languages: require("./src/languages"),
+    Maps: require("./src/maps"),
+    Regions: require("./src/regions"),
+    Tiers: require("./src/tiers"),
+  
+};
