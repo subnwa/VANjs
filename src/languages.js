004062ea91382c5cb50c151b84f905d3cae9ce14
nwa <nwa.bio.web@gmail.com>
2022-07-07 20:18:05 +0300

    src/Static

---
+++ src/languages.js
@@ -0,0 +1,20 @@
+const languages = {
+    English: "en-US",
+    Turkish: "tr-TR",
+    Italien: "it-IT",
+    Thai: "th-TH",
+    Vietnamese: "vi-VN",
+    Chinese_Simplified: "zh-CN",
+    Chinese_Traditional: "zh-TW",
+    Arabic: "ar-AE",
+    German: "de-DE",
+    Spanish_Spain: "es-ES",
+    Spanish_Mexico: "es-MX",
+    Japanese: "ja-JP",
+    Korean: "ko-KR",
+    Polish: "pl-PL",
+    Portuguese_Brazil: "pt-BR",
+    Russian: "ru-RU",
+};
+
+module.exports = languages;
